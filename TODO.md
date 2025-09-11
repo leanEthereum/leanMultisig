bd372c7b017a54e1e29d0457a5c8d6dc839a170d
Tom Wambsgans <TomWambsgans@users.noreply.github.com>
2025-09-11 18:11:19 +0400

    wip

--- TODO.md
+++ TODO.md
@@ -25,6 +25,8 @@
 - the 2 executions of the program, before generating the validity proof, can be merged, using some kind of placeholders
 - both WHIR verif + XMSS aggregation programs have 40% of unused memory!! -> TODO improve the compiler to reduce memory fragmentation
 - Many times, we evaluate different multilinear polynomials (diferent columns of the same table etc) at a common point. OPTI = compute the eq(.) once, and then dot_product with everything
+- To commit to multiple AIR table using 1 single pcs, the most general form our "packed pcs" api should accept is:
+ a list of n (n not a power of 2) columns, each ending with m repeated values (in this manner we can reduce proof size when they are a lot of columns (poseidons ...))
 
 ## Not Perf
 
--- crates/pcs/src/packed_pcs.rs
+++ crates/pcs/src/packed_pcs.rs
@@ -1,72 +1,214 @@
+use std::{cmp::Reverse, collections::BTreeMap};
+
 use p3_field::{ExtensionField, Field};
-use p3_util::log2_strict_usize;
+use p3_util::{log2_ceil_usize, log2_strict_usize};
+use rayon::prelude::*;
 use tracing::instrument;
-use utils::{Evaluation, FSProver, FSVerifier, PF};
+use utils::{Evaluation, FSProver, FSVerifier, PF, to_big_endian_in_field};
+use whir_p3::poly::evals::EvaluationsList;
 use whir_p3::{
     dft::EvalsDft,
     fiat_shamir::{FSChallenger, errors::ProofError},
     poly::multilinear::MultilinearPoint,
 };
 
-use crate::{
-    PCS,
-    combinatorics::{TreeOfVariables, TreeOfVariablesInner},
-};
+use crate::PCS;
 
-#[derive(Debug)]
+const LOG_SMALLEST_DECOMPOSITION_CHUNK: usize = 10;
+
+#[derive(Debug, Clone)]
+struct CommittedChunk {
+    original_poly_index: usize,
+    vars: usize,
+    index_in_original: usize, // 0 if first (biggest) chunk, 1 if second, etc.
+    offset_in_packed: Option<usize>,
+}
+
+#[derive(Debug, Clone)]
 pub struct MultiCommitmentWitness<F: Field, EF: ExtensionField<F>, Pcs: PCS<F, EF>> {
-    pub tree: TreeOfVariables,
+    chunks_decomposition: BTreeMap<usize, Vec<CommittedChunk>>, // real polynomial index -> list of virtual polynomial indexes (chunks) in which it was split
     pub inner_witness: Pcs::Witness,
     pub packed_polynomial: Vec<F>,
 }
 
-pub fn num_packed_vars_for_pols<F: Field>(polynomials: &[&[F]]) -> usize {
-    let vars_per_polynomial = polynomials
-        .iter()
-        .map(|p| log2_strict_usize(p.len()))
-        .collect::<Vec<_>>();
-    TreeOfVariables::compute_optimal(vars_per_polynomial).total_vars()
+#[derive(Debug, Clone, Copy)]
+pub struct CommittedPol<'a, F: Field> {
+    pub polynomial: &'a [F],
+    pub vars: usize,            // polynomial.len() == 1 << vars
+    pub relevant_length: usize, // polynomial[relevant_length..] = [default_value, ..., default_value]
+    pub default_value: F,
 }
 
-pub fn num_packed_vars_for_vars(vars_per_polynomial: &[usize]) -> usize {
-    TreeOfVariables::compute_optimal(vars_per_polynomial.to_vec()).total_vars()
+impl<'a, F: Field> CommittedPol<'a, F> {
+    pub fn dense(polynomial: &'a [F]) -> Self {
+        Self {
+            polynomial,
+            vars: log2_strict_usize(polynomial.len()),
+            relevant_length: polynomial.len(),
+            default_value: F::ZERO,
+        }
+    }
+
+    pub fn sparse(polynomial: &'a [F], relevant_length: usize, default_value: F) -> Self {
+        assert!(relevant_length <= polynomial.len());
+        debug_assert!(
+            polynomial[relevant_length..]
+                .par_iter()
+                .all(|c| c == &default_value)
+        );
+        Self {
+            polynomial,
+            vars: log2_strict_usize(polynomial.len()),
+            relevant_length,
+            default_value,
+        }
+    }
+}
+
+fn split_in_chunks<F: Field>(poly_index: usize, poly: &CommittedPol<'_, F>) -> Vec<CommittedChunk> {
+    let mut remaining = poly.relevant_length;
+    let mut index_in_original = 0;
+    let mut res = Vec::new();
+    loop {
+        let chunk_size =
+            if remaining.next_power_of_two() - remaining <= 1 << LOG_SMALLEST_DECOMPOSITION_CHUNK {
+                log2_ceil_usize(remaining)
+            } else {
+                remaining.ilog2() as usize
+            };
+        res.push(CommittedChunk {
+            original_poly_index: poly_index,
+            vars: chunk_size,
+            index_in_original,
+            offset_in_packed: None,
+        });
+        index_in_original += 1;
+        if remaining <= 1 << chunk_size {
+            return res;
+        }
+        remaining -= 1 << chunk_size;
+    }
 }
 
 #[instrument(skip_all)]
 pub fn packed_pcs_commit<F: Field, EF: ExtensionField<F>, Pcs: PCS<F, EF>>(
     pcs: &Pcs,
-    polynomials: &[&[F]],
+    polynomials: &[CommittedPol<'_, F>],
     dft: &EvalsDft<PF<EF>>,
     prover_state: &mut FSProver<EF, impl FSChallenger<EF>>,
 ) -> MultiCommitmentWitness<F, EF, Pcs> {
-    let polynomials: Vec<&[F]> = polynomials.to_vec();
-    let vars_per_polynomial = polynomials
+    let mut all_chunks = Vec::new();
+    for (i, poly) in polynomials.iter().enumerate() {
+        all_chunks.extend(split_in_chunks(i, poly));
+    }
+    all_chunks.sort_by_key(|c| Reverse(c.vars));
+
+    let mut offset_in_packed = 0;
+    let mut chunks_decomposition: BTreeMap<usize, Vec<CommittedChunk>> = BTreeMap::new();
+    for chunk in &mut all_chunks {
+        chunk.offset_in_packed = Some(offset_in_packed);
+        offset_in_packed += 1 << chunk.vars;
+        chunks_decomposition
+            .entry(chunk.original_poly_index)
+            .or_default()
+            .push(chunk.clone());
+    }
+
+    let total_size = all_chunks
         .iter()
-        .map(|p| log2_strict_usize(p.len()))
-        .collect::<Vec<_>>();
-    let tree = TreeOfVariables::compute_optimal(vars_per_polynomial);
-    let mut packed_polynomial = F::zero_vec(1 << tree.total_vars());
-    tree.root.fill_commitment(
-        &mut packed_polynomial,
-        &polynomials,
-        &tree.vars_per_polynomial,
-    );
+        .map(|c| 1 << c.vars)
+        .sum::<usize>()
+        .next_power_of_two();
+    let packed_polynomial = F::zero_vec(total_size);
+    all_chunks.par_iter().for_each(|chunk| {
+        let start = chunk.offset_in_packed.unwrap();
+        let end = start + (1 << chunk.vars);
+        let original_poly = &polynomials[chunk.original_poly_index].polynomial;
+        // original_start = (1 << (chunk.vars + 1)) + (1 << (chunk.vars + 2)) + ... (1 << (chunk.vars + index_in_original - 1))
+        let original_start = (1 << (chunk.vars + 1)) * ((1 << chunk.index_in_original) - 1);
+        let original_end = original_start + (1 << chunk.vars);
+        unsafe {
+            let slice = std::slice::from_raw_parts_mut(
+                (packed_polynomial.as_ptr() as *mut F).add(start),
+                end - start,
+            );
+            slice.copy_from_slice(&original_poly[original_start..original_end]);
+        }
+    });
+
     let inner_witness = pcs.commit(dft, prover_state, &packed_polynomial);
     MultiCommitmentWitness {
-        tree,
+        chunks_decomposition,
         inner_witness,
         packed_polynomial,
     }
 }
 
-pub fn packed_pcs_global_statements<EF: Field>(
-    tree: &TreeOfVariables,
+pub fn packed_pcs_global_statements<
+    F: Field,
+    EF: ExtensionField<F> + ExtensionField<PF<EF>>,
+    Pcs: PCS<F, EF>,
+>(
+    witness: &MultiCommitmentWitness<F, EF, Pcs>,
     statements_per_polynomial: &[Vec<Evaluation<EF>>],
+    prover_state: &mut FSProver<EF, impl FSChallenger<EF>>,
 ) -> Vec<Evaluation<EF>> {
-    check_tree(&tree, statements_per_polynomial).expect("Invalid tree structure for multi-open");
+    // TODO:
+    // 1) parallelize
+    // 2) cache the "eq" poly, and then use dot product
+    // 3) for each statement we can avoid one of the sub-computation (so we should avoid the big one)
 
-    tree.root
-        .global_statement(&tree.vars_per_polynomial, statements_per_polynomial, &[])
+    let packed_vars = log2_strict_usize(witness.packed_polynomial.len());
+    let mut packed_statements = Vec::new();
+    for (poly_index, statements) in statements_per_polynomial.iter().enumerate() {
+        let chunks = &witness.chunks_decomposition[&poly_index];
+        assert!(!chunks.is_empty());
+        for statement in statements {
+            if chunks.len() == 1 {
+                assert_eq!(chunks[0].vars, statement.point.0.len());
+                let packed_point = MultilinearPoint(
+                    [
+                        to_big_endian_in_field(
+                            chunks[0].offset_in_packed.unwrap(),
+                            packed_vars - chunks[0].vars,
+                        ),
+                        statement.point.0.clone(),
+                    ]
+                    .concat(),
+                );
+                packed_statements.push(Evaluation {
+                    point: packed_point,
+                    value: statement.value,
+                });
+            } else {
+                for (chunk_index, chunk) in chunks.iter().enumerate() {
+                    let sub_point = MultilinearPoint(statement.point.0[1 + chunk_index..].to_vec());
+                    let sub_value = (&witness.packed_polynomial[chunk.offset_in_packed.unwrap()
+                        << chunk.vars
+                        ..(chunk.offset_in_packed.unwrap() + 1) << chunk.vars])
+                        .evaluate(&sub_point);
+                    let packed_point = MultilinearPoint(
+                        [
+                            to_big_endian_in_field(
+                                chunk.offset_in_packed.unwrap(),
+                                packed_vars - chunk.vars,
+                            ),
+                            sub_point.0.clone(),
+                        ]
+                        .concat(),
+                    );
+                    packed_statements.push(Evaluation {
+                        point: packed_point,
+                        value: sub_value,
+                    });
+                    prover_state.add_extension_scalar(sub_value);
+
+                    // TODO sanity assert_eq
+                }
+            }
+        }
+    }
+    packed_statements
 }
 
 #[derive(Debug)]
@@ -87,97 +229,6 @@ pub fn packed_pcs_parse_commitment<F: Field, EF: ExtensionField<F>, Pcs: PCS<F, 
         inner_parsed_commitment,
     })
 }
-
-fn check_tree<EF: Field>(
-    tree: &TreeOfVariables,
-    statements_per_polynomial: &[Vec<Evaluation<EF>>],
-) -> Result<(), ProofError> {
-    assert_eq!(
-        statements_per_polynomial.len(),
-        tree.vars_per_polynomial.len()
-    );
-    for (evals, &vars) in statements_per_polynomial
-        .iter()
-        .zip(&tree.vars_per_polynomial)
-    {
-        for eval in evals {
-            assert_eq!(eval.point.len(), vars);
-        }
-    }
-    Ok(())
-}
-
-impl TreeOfVariablesInner {
-    fn fill_commitment<F: Field>(
-        &self,
-        buff: &mut [F],
-        polynomials: &[&[F]],
-        vars_per_polynomial: &[usize],
-    ) {
-        match self {
-            TreeOfVariablesInner::Polynomial(i) => {
-                let len = polynomials[*i].len();
-                buff[..len].copy_from_slice(polynomials[*i]);
-            }
-            TreeOfVariablesInner::Composed { left, right } => {
-                let (left_buff, right_buff) = buff.split_at_mut(buff.len() / 2);
-                let left_buff = &mut left_buff[..1 << left.total_vars(vars_per_polynomial)];
-                let right_buff = &mut right_buff[..1 << right.total_vars(vars_per_polynomial)];
-                rayon::join(
-                    || left.fill_commitment(left_buff, polynomials, vars_per_polynomial),
-                    || right.fill_commitment(right_buff, polynomials, vars_per_polynomial),
-                );
-            }
-        }
-    }
-
-    fn global_statement<F: Field, EF: ExtensionField<F>>(
-        &self,
-        vars_per_polynomial: &[usize],
-        statements_per_polynomial: &[Vec<Evaluation<EF>>],
-        selectors: &[EF],
-    ) -> Vec<Evaluation<EF>> {
-        match self {
-            TreeOfVariablesInner::Polynomial(i) => {
-                let mut res = Vec::new();
-                for eval in &statements_per_polynomial[*i] {
-                    res.push(Evaluation {
-                        point: MultilinearPoint(
-                            [selectors.to_vec(), eval.point.0.clone()].concat(),
-                        ),
-                        value: eval.value,
-                    });
-                }
-                res
-            }
-            TreeOfVariablesInner::Composed { left, right } => {
-                let left_vars = left.total_vars(vars_per_polynomial);
-                let right_vars = right.total_vars(vars_per_polynomial);
-
-                let mut left_selectors = selectors.to_vec();
-                left_selectors.push(EF::ZERO);
-                left_selectors.extend(EF::zero_vec(right_vars.saturating_sub(left_vars)));
-
-                let mut right_selectors = selectors.to_vec();
-                right_selectors.push(EF::ONE);
-                right_selectors.extend(EF::zero_vec(left_vars.saturating_sub(right_vars)));
-
-                let left_statements = left.global_statement(
-                    vars_per_polynomial,
-                    statements_per_polynomial,
-                    &left_selectors,
-                );
-                let right_statements = right.global_statement(
-                    vars_per_polynomial,
-                    statements_per_polynomial,
-                    &right_selectors,
-                );
-                [left_statements, right_statements].concat()
-            }
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use std::marker::PhantomData;
