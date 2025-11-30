cccf7506c56271c5b58da727712e000d3a679568
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2025-11-30 03:57:58 +0000

    improve specificity of MemoryAlreadySet error message

--- crates/lean_compiler/src/lib.rs
+++ crates/lean_compiler/src/lib.rs
@@ -24,8 +24,9 @@ pub fn compile_program(program: String) -> Bytecode {
     // for (loc, name) in function_locations.iter() {
     //     println!("{name}: {loc}");
     // }
-    // println!("\n\nCompiled Program:\n\n{compiled}");
-    compile_to_low_level_bytecode(intermediate_bytecode, program, function_locations).unwrap()
+    let compiled = compile_to_low_level_bytecode(intermediate_bytecode, program, function_locations).unwrap();
+    println!("\n\nCompiled Program:\n\n{compiled}");
+    compiled
 }
 
 pub fn compile_and_run(
