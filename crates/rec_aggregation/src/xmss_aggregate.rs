84f335988a6f6c7519b0054fe74faf7af6e9e36e
Tom Wambsgans <TomWambsgans@users.noreply.github.com>
2025-12-17 18:40:33 +0100

    test_config

--- crates/rec_aggregation/src/xmss_aggregate.rs
+++ crates/rec_aggregation/src/xmss_aggregate.rs
@@ -1,11 +1,10 @@
 use lean_compiler::*;
 use lean_prover::{prove_execution::prove_execution, verify_execution::verify_execution};
 use lean_vm::*;
-use leansig::signature::generalized_xmss::instantiations_poseidon_top_level::lifetime_2_to_the_32::hashing_optimized as leansig_module;
-use leansig::signature::{SignatureScheme, SignatureSchemeSecretKey};
+use leansig::signature::SignatureScheme;
+use leansig::signature::SignatureSchemeSecretKey;
 use leansig::symmetric::message_hash::MessageHash;
 use leansig::symmetric::tweak_hash::poseidon::PoseidonTweak;
-use leansig_module::{HASH_LEN_FE, MH, RAND_LEN_FE, TWEAK_LEN_FE};
 use multilinear_toolkit::prelude::*;
 use rand::{Rng, SeedableRng, rngs::StdRng};
 use std::sync::OnceLock;
@@ -14,9 +13,16 @@ use std::{mem::transmute, path::Path};
 use tracing::{info_span, instrument};
 use whir_p3::precompute_dft_twiddles;
 
-pub type LeanSigScheme = leansig_module::SIGTopLevelTargetSumLifetime32Dim64Base8;
-pub type LeanSigPubKey = leansig_module::PubKeyTopLevelTargetSumLifetime32Dim64Base8;
-pub type LeanSigSignature = leansig_module::SigTopLevelTargetSumLifetime32Dim64Base8;
+#[cfg(feature = "test_config")]
+pub use crate::wrappers::test_config as config;
+
+#[cfg(not(feature = "test_config"))]
+pub use crate::wrappers::prod_config as config;
+
+use config::{
+    BASE, CAPACITY, DIMENSION, HASH_LEN_FE, IS_PROD_CONFIG, LOG_LIFETIME, LeanSigPubKey, LeanSigScheme,
+    LeanSigSignature, MH, PARAMETER_LEN, RAND_LEN_FE, TWEAK_LEN_FE,
+};
 
 static XMSS_AGGREGATION_PROGRAM: OnceLock<Bytecode> = OnceLock::new();
 
@@ -36,8 +42,8 @@ fn build_public_input(
 ) -> Vec<F> {
     assert_eq!(pub_keys.len(), encoding_randomness.len());
     let mut public_input = vec![F::from_usize(pub_keys.len())];
-    for chain_index in 0..MH::DIMENSION as u8 {
-        for pos_in_chain in 1..MH::BASE as u8 {
+    for chain_index in 0..DIMENSION as u8 {
+        for pos_in_chain in 1..BASE as u8 {
             let chain_tweak = PoseidonTweak::ChainTweak {
                 epoch,
                 chain_index,
@@ -57,12 +63,9 @@ fn build_public_input(
 
     for (pub_key, randomness) in pub_keys.iter().zip(encoding_randomness.iter()) {
         let encoding = MH::apply(pub_key.parameter(), epoch, unsafe { transmute(randomness) }, message);
-        assert_eq!(encoding.len(), MH::DIMENSION);
-        assert_eq!(MH::DIMENSION, 64); // TODO remove this later
+        assert_eq!(encoding.len(), DIMENSION);
         let merkle_root = unsafe { transmute::<_, Vec<F>>(pub_key.root().to_vec()) };
-        assert_eq!(merkle_root.len(), 8);
         let public_param = unsafe { transmute::<_, Vec<F>>(pub_key.parameter().to_vec()) };
-        assert_eq!(public_param.len(), 5);
 
         public_input.extend(merkle_root);
         public_input.extend(public_param);
@@ -76,7 +79,6 @@ fn build_private_input(all_signatures: &[LeanSigSignature]) -> Vec<F> {
     for signature in all_signatures {
         let chain_tips = unsafe { transmute::<_, Vec<[F; HASH_LEN_FE]>>(signature.hashes().clone()) };
         let merkle_path = unsafe { transmute::<_, Vec<[F; HASH_LEN_FE]>>(signature.path().clone()) };
-        assert_eq!(chain_tips.len(), 64); // TODO remove this later
         assert_eq!(merkle_path.len(), LeanSigScheme::LIFETIME.ilog2() as usize);
         private_input.extend(chain_tips.into_iter().flatten());
         private_input.extend(merkle_path.into_iter().flatten());
@@ -87,7 +89,32 @@ fn build_private_input(all_signatures: &[LeanSigSignature]) -> Vec<F> {
 #[instrument(skip_all)]
 fn compile_xmss_aggregation_program() -> Bytecode {
     let src_file = Path::new(env!("CARGO_MANIFEST_DIR")).join("xmss_aggregate.lean_lang");
-    let program_str = std::fs::read_to_string(src_file).unwrap();
+    let merkle_leaf_len_fe = PARAMETER_LEN + TWEAK_LEN_FE + DIMENSION * HASH_LEN_FE;
+    let sponge_rate_fe = 24 - CAPACITY;
+    let sponge_n_perms = merkle_leaf_len_fe.div_ceil(sponge_rate_fe);
+    let sponge_final_zero_padding = (sponge_n_perms * sponge_rate_fe) - merkle_leaf_len_fe;
+    let sponge_capacity = if IS_PROD_CONFIG {
+        vec![
+            287609684, 1664498138, 719484663, 1366363664, 1775736341, 1392984152, 1281304957, 1948506587, 660369959,
+        ]
+    } else {
+        vec![
+            1665285720, 695566739, 283798675, 1389712078, 1693509235, 1598839968, 1965072165, 1925594806, 1228158567,
+        ]
+    };
+    let mut program_str = std::fs::read_to_string(src_file)
+        .unwrap()
+        .replace("W_PLACEHOLDER", &BASE.to_string())
+        .replace("LOG_LIFETIME_PLACEHOLDER", &LOG_LIFETIME.to_string())
+        .replace("SPONGE_N_PERMS_PLACEHOLDER", &sponge_n_perms.to_string())
+        .replace(
+            "SPONGE_FINAL_ZERO_PADDING_PLACEHOLDER",
+            &sponge_final_zero_padding.to_string(),
+        )
+        .replace("V_PLACEHOLDER", &DIMENSION.to_string());
+    for (i, cap) in sponge_capacity.iter().enumerate() {
+        program_str = program_str.replace(&format!("CAP_{}_PLACEHOLDER", i), &cap.to_string());
+    }
     compile_program(program_str)
 }
 
@@ -120,9 +147,12 @@ pub fn run_xmss_benchmark(n_xmss: usize, tracing: bool) {
 
     let mut rng = StdRng::seed_from_u64(0);
     let message: [u8; 32] = rng.random();
-    let epoch: u32 = 1000;
+    let epoch: u32 = 11;
 
-    let log_lifetimes = (0..n_xmss).map(|_| rng.random_range(5..10)).collect::<Vec<u32>>();
+    let lifetime_range = if IS_PROD_CONFIG { 5..10 } else { 3..7 };
+    let log_lifetimes = (0..n_xmss)
+        .map(|_| rng.random_range(lifetime_range.clone()))
+        .collect::<Vec<u32>>();
 
     let mut pub_keys = Vec::new();
     let mut signatures = Vec::new();
@@ -130,7 +160,8 @@ pub fn run_xmss_benchmark(n_xmss: usize, tracing: bool) {
         let activation_epoch = epoch - log_lifetime;
 
         let file_name = format!(
-            "xmss_example_{}_{}_{}_{}.bin",
+            "xmss_example_{}_{}_{}_{}_{}.bin",
+            IS_PROD_CONFIG,
             log_lifetime,
             activation_epoch,
             epoch,
