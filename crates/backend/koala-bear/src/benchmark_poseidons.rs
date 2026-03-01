b40774ed524cee0a03b4871f8e011a18c073ce2a
Tom Wambsgans <TomWambsgans@users.noreply.github.com>
2026-03-01 17:00:37 +0400

    wip

--- crates/backend/koala-bear/src/benchmark_poseidons.rs
+++ crates/backend/koala-bear/src/benchmark_poseidons.rs
@@ -3,7 +3,6 @@ use std::time::Instant;
 use field::Field;
 use field::PackedValue;
 use field::PrimeCharacteristicRing;
-use rayon::prelude::*;
 
 use crate::{KoalaBear, Poseidon1KoalaBear16, default_koalabear_poseidon2_16};
 
@@ -64,31 +63,5 @@ fn bench_koalabear_1_vs_2_plaintext() {
         "Poseidon2, single-threaded, SIMD: {:.2}M hashes / s ({:.1}x faster than Poseidon1)",
         (n as f64 / time_p2_simd.as_secs_f64() / 1_000_000.0),
         (time_p1_simd.as_secs_f64() / time_p2_simd.as_secs_f64())
-    );
-
-    // SIMD, multi-threaded
-    let time = Instant::now();
-    (0..n / PACKING_WIDTH).into_par_iter().for_each(|i| {
-        let mut state = [FPacking::ZERO; 16];
-        state[0] = FPacking::from_usize(i);
-        poseidon1.compress_in_place(&mut state);
-    });
-    let time_p1_simd_mt = time.elapsed();
-    println!(
-        "Poseidon1, multi-threaded, SIMD: {:.2}M hashes / s",
-        (n as f64 / time_p1_simd_mt.as_secs_f64() / 1_000_000.0)
-    );
-
-    let time = Instant::now();
-    (0..n / PACKING_WIDTH).into_par_iter().for_each(|i| {
-        let mut state = [FPacking::ZERO; 16];
-        state[0] = FPacking::from_usize(i);
-        poseidon2.compress_in_place(&mut state);
-    });
-    let time_p2_simd_mt = time.elapsed();
-    println!(
-        "Poseidon2, multi-threaded, SIMD: {:.2}M hashes / s ({:.1}x faster than Poseidon1)",
-        (n as f64 / time_p2_simd_mt.as_secs_f64() / 1_000_000.0),
-        (time_p1_simd_mt.as_secs_f64() / time_p2_simd_mt.as_secs_f64())
     );
 }
